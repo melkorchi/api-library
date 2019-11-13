976cb03462af408bce0bcad4b43aed00f9a08892
melkorchi <logimek72@gmail.com>
2019-11-13 12:16:13 +0100

    use heroku config env

--- src/environnement.js
+++ src/environnement.js
@@ -1,11 +1,12 @@
 module.exports = {
     env: false,
     url: "localhost",
-    port: 3000,
+    port: process.env.PORT || 3000,
     bdd: {
         mongo: {
             // url: "mongodb://localhost/library"
-            url: "mongodb+srv://<username>:<password>@cluster0-webya.mongodb.net/test?retryWrites=true&w=majority"
+            //url: "mongodb+srv://<username>:<password>@cluster0-webya.mongodb.net/test?retryWrites=true&w=majority"
+            url: process.env.MONGODB_URI
         }
     },
     smtp: {
